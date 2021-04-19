b41493e578f594e1c1e61373e1c88194fce174fb
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-04-19 19:42:15 +0300

    1 päivä

---
+++ EmployeePage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using XamarinTimesheet2021.Models;
+using System.Collections.ObjectModel;
+
+namespace XamarinTimesheet2021
+{
+    public partial class EmployeePage : ContentPage
+    {
+        public EmployeePage()
+        {
+            InitializeComponent();
+
+            //Annetaan latausilmoitus
+            emp_lataus.Text = "Ladataan työntekijöitä...";
+
+            LoadDataFromRestAPI();
+
+            async void LoadDataFromRestAPI()
+            {
+                try
+                {
+                    HttpClient client = new HttpClient();
+
+                    client.BaseAddress = new Uri("https://timesheetbackend2021.azurewebsites.net/");
+                    string json = await client.GetStringAsync("/api/employees/");
+
+                    IEnumerable<Employee> employees = JsonConvert.DeserializeObject<Employee[]>(json);
+                    ObservableCollection<Employee> dataa = new ObservableCollection<Employee>(employees);
+                    employeeList.ItemsSource = dataa;
+
+                    // Tyhjennetään latausilmoitus label
+                    emp_lataus.Text = "";
+                }
+                catch (Exception e)
+                {
+                    await DisplayAlert("Virhe", e.Message.ToString(), "SELVÄ!");
+                }
+            }
+
+        }
+
+
+        async void navbutton_Clicked(object sender, EventArgs e)
+        {
+            Employee emp = (Employee)employeeList.SelectedItem;
+           
+            if (emp == null)
+            {
+                await DisplayAlert("Valinta puuttuu", "Valitse työntekijä.", "OK"); // (otsikko, teksti, kuittausnapin teksti)
+            }
+            else
+            {
+                //await DisplayAlert("Valittu", emp.FirstName, "OK");
+
+                int id = emp.IdEmployee;
+                await Navigation.PushAsync(new WorkAssignmentPage(id)); // Navigoidaan uudelle sivulle
+            }
+        }
+
+    }
+ }
+
---
+++ WorkAssignmentPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections.ObjectModel;
+using XamarinTimesheet2021.Models;
+
+namespace XamarinTimesheet2021
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WorkAssignmentPage : ContentPage
+    {
+        public WorkAssignmentPage(int id)
+        {
+            InitializeComponent();
+
+            //Annetaan latausilmoitus
+            työ_lataus.Text = "Ladataan työtehtäviä...";
+
+            LoadDataFromRestAPI();
+
+            async void LoadDataFromRestAPI()
+            {
+                try
+                {
+
+                    workList.ItemsSource = new List<string> { "Ladataan", "Työtehtäviä", "palvelimelta..." };
+
+                    HttpClient client = new HttpClient();
+
+                    client.BaseAddress = new Uri("https://timesheetbackend2021.azurewebsites.net/");
+                    string json = await client.GetStringAsync("/api/workassignments/");
+
+                    IEnumerable<WorkAssignment> works = JsonConvert.DeserializeObject<WorkAssignment[]>(json);
+                    ObservableCollection<WorkAssignment> dataa = new ObservableCollection<WorkAssignment>(works);
+                    workList.ItemsSource = dataa;
+
+                    //Poistetaan latausilmoitus
+                    työ_lataus.Text = "";
+                }
+                catch (Exception e)
+                {
+                    await DisplayAlert("Virhe", e.Message.ToString(), "SELVÄ!");
+                }
+            }
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Aloitus_Nappi_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lopetus_Nappi_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
