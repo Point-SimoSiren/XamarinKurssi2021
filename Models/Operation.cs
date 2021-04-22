4d3bd714645d75f10d01ad679b141da4eca9d356
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-04-22 14:17:50 +0300

    pv3

---
+++ Models/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinTimesheet2021.Models
+{
+    public class Operation
+    {
+        public int EmployeeID { get; set; }
+        public int? CustomerID { get; set; }
+        public int WorkAssignmentID { get; set; }
+        public string OperationType { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
--- WorkAssignmentPage.xaml.cs
+++ WorkAssignmentPage.xaml.cs
@@ -15,9 +15,13 @@ namespace XamarinTimesheet2021
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WorkAssignmentPage : ContentPage
     {
-        public WorkAssignmentPage(int id)
+        int eId;
+
+        public WorkAssignmentPage(int idParam)
         {
             InitializeComponent();
+
+            eId = idParam;
 
             //Annetaan latausilmoitus
             työ_lataus.Text = "Ladataan työtehtäviä...";
@@ -51,18 +55,128 @@ namespace XamarinTimesheet2021
 
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+
+        async void Aloitus_Nappi_Clicked(object sender, EventArgs e)
         {
+            WorkAssignment wa = (WorkAssignment)workList.SelectedItem;
+
+            if (wa == null)
+            {
+                await DisplayAlert("Valinta puuttuu", "Valitse työtehtävä.", "OK");
+            }
+
+            try
+            {
+                Operation op = new Operation
+                {
+                    EmployeeID = eId,
+                    WorkAssignmentID = wa.IdWorkAssignment,
+                    CustomerID = wa.IdCustomer,
+                    OperationType = "start",
+                    Comment = "Aloitettu"
+                };
+
+                HttpClient client = new HttpClient();
+
+                client.BaseAddress = new Uri("https://timesheetbackend2021.azurewebsites.net/");
+
+                // Muutetaan em. data objekti Jsoniksi
+                string input = JsonConvert.SerializeObject(op);
+                StringContent content = new StringContent(input, Encoding.UTF8, "application/json");
+
+
+                // Lähetetään serialisoitu objekti back-endiin Post pyyntönä
+                HttpResponseMessage message = await client.PostAsync("/api/workassignments", content);
+
+
+                // Otetaan vastaan palvelimen vastaus
+                string reply = await message.Content.ReadAsStringAsync();
+
+
+                //Asetetaan vastaus serialisoituna success muuttujaan
+                bool success = JsonConvert.DeserializeObject<bool>(reply);
+
+                if (success == false)
+                {
+                    await DisplayAlert("Ei voida aloittaa", "Työ on jo käynnissä", "OK");
+                }
+                else if (success == true)
+                {
+                    await DisplayAlert("Työ aloitettu", "Työ on aloitettu", "OK");
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                await DisplayAlert(ex.GetType().Name, ex.Message, "OK");
+            }
 
         }
 
-        private void Aloitus_Nappi_Clicked(object sender, EventArgs e)
+        async void Lopetus_Nappi_Clicked(object sender, EventArgs e)
         {
 
-        }
+            WorkAssignment wa = (WorkAssignment)workList.SelectedItem;
+            
+            if (wa == null)
+            {
+                await DisplayAlert("Valinta puuttuu", "Valitse työtehtävä.", "OK");
+                return;
+            }
 
-        private void Lopetus_Nappi_Clicked(object sender, EventArgs e)
-        {
+            string result = await DisplayPromptAsync("Kommentti", "Kirjoita kommentti");
+
+            try
+            {
+                Operation op = new Operation
+                {
+                    EmployeeID = eId,
+                    WorkAssignmentID = wa.IdWorkAssignment,
+                    CustomerID = wa.IdCustomer,
+                    OperationType = "stop",
+                    Comment = result
+                    
+                };
+
+                HttpClient client = new HttpClient();
+
+                client.BaseAddress = new Uri("https://timesheetbackend2021.azurewebsites.net/");
+
+                // Muutetaan em. data objekti Jsoniksi
+                string input = JsonConvert.SerializeObject(op);
+                StringContent content = new StringContent(input, Encoding.UTF8, "application/json");
+
+
+                // Lähetetään serialisoitu objekti back-endiin Post pyyntönä
+                HttpResponseMessage message = await client.PostAsync("/api/workassignments", content);
+
+
+                // Otetaan vastaan palvelimen vastaus
+                string reply = await message.Content.ReadAsStringAsync();
+
+
+                //Asetetaan vastaus serialisoituna success muuttujaan
+                bool success = JsonConvert.DeserializeObject<bool>(reply);
+
+                if (success == false)
+                {
+                    await DisplayAlert("Ei voida lopettaa", "Työtä ei ole aloitettu", "OK");
+                }
+
+                else if (success == true)
+                {
+                    await DisplayAlert("Työn päättyminen", "Työ on päättynyt", "OK");
+
+                    await Navigation.PushAsync(new WorkAssignmentPage(eId));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert(ex.GetType().Name, ex.Message, "OK");
+            }
+
 
         }
     }
